d15bfc80f102e446f58c51a6f2081aaf36671896
Mark <markcartwright65@gmail.com>
2023-01-24 21:21:12 +0000

    Fix call to super in init method

--- code/tile.py
+++ code/tile.py
@@ -3,7 +3,7 @@ from settings import *
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos: tuple[float, float], surf: pygame.Surface, groups: pygame.sprite.Group, tiled_layer: str) -> None:
-        super.__init__(groups)
+        super().__init__(groups)
         self.tiled_layer = tiled_layer
         self.image = pygame.transform.scale(surf, (TILE_SIZE, TILE_SIZE))
         self.rect = self.image.get_rect(topleft=pos)
