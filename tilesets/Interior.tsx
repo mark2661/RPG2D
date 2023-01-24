8b89a22a8f540274f231d8bbd2a8e3215480431d
Mark <markcartwright65@gmail.com>
2023-01-24 20:51:14 +0000

    Add Overworld and Interior tilesets to tilesets/

---
+++ tilesets/Interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Interior" tilewidth="16" tileheight="16" tilecount="1000" columns="40">
+ <image source="../../pygame/test/assests/Inner.png" width="640" height="400"/>
+</tileset>
