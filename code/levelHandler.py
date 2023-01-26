7e7e344b82afbcef99048ba59bba381ccc1fe561
Mark <markcartwright65@gmail.com>
2023-01-26 12:45:19 +0000

    Add levelHandler.py

---
+++ code/levelHandler.py
@@ -0,0 +1,38 @@
+import os
+
+import pygame.display
+
+from settings import *
+from level import Level
+from player import Player
+
+
+class LevelHandler:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+
+        self.levels = {
+                        0: Level(os.path.join(MAPS_FILE_PATH, "0.tmx")),
+                        1: Level(os.path.join(MAPS_FILE_PATH, "1.tmx"))
+                      }
+
+        # initialise current level to the starting level
+        self.current_level_code = 0
+        self.current_level = self.levels[self.current_level_code]
+
+        # get the pygame group member objects of the current level
+        self.visible_sprites_group, self.obstacle_sprites_group,\
+            self.transition_sprites_group = self.current_level.get_level_groups()
+
+        # create the player instance that will be passed between levels
+        player_spawn_position = tuple(map(lambda x: x // 2, self.display_surface.get_size()))
+        player_spawn_image_file_path = os.path.join(PLAYER_IMAGES_FILE_PATH, "down_idle", "down_idle_1.png")
+        self.player = Player(player_spawn_position, player_spawn_image_file_path, [self.visible_sprites_group],
+                             self.obstacle_sprites_group, self.transition_sprites_group, self.current_level_code)
+
+        self.current_level.set_player(self.player)
+
+    def run(self):
+        self.current_level.run()
+
+
