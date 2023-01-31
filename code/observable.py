47f72f94d2619bc2298bb90b9455c52ad099440f
Mark <markcartwright65@gmail.com>
2023-01-31 20:07:08 +0000

    Fix cyclic import bug in Observer and Observable classes
    change typehint to a string containing the type name

--- code/observable.py
+++ code/observable.py
@@ -1,14 +1,16 @@
-from observer import Observer
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from observer import Observer
 
 
 class Observable:
     def __init__(self):
         self.observers = []
 
-    def observable_add(self, new_observer: Observer):
+    def observable_add(self, new_observer: "Observer"):
         self.observers.append(new_observer)
 
-    def observable_remove(self, observer_to_delete: Observer):
+    def observable_remove(self, observer_to_delete: "Observer"):
         for index, observer in enumerate(self.observers):
             if id(observer) == id(observer_to_delete):
                 self.observers.pop(index)
--- code/observer.py
+++ code/observer.py
@@ -1,8 +1,10 @@
-from observable import Observable
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from observable import Observable
 
 
 class Observer:
-    def __init__(self, observable: Observable):
+    def __init__(self, observable: "Observable"):
         self.observable = observable
 
     def update(self):
