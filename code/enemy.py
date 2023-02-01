e00814f0958aab78123a1142af65cffe20c0ed53
Mark <markcartwright65@gmail.com>
2023-02-01 17:57:34 +0000

    Add test enemy to levels
    level.py
    
    added create_enemies function to create a test Enemy object in a fixed
    position
    
    fixed a bug in the conditionals in floor_tiles and non_floor tiles in YSortCameraGroup
    to use isinstance to check if a sprite is an Entity instead of a just a
    Player. type() was causing the sprite.tiled_layer check to access an
    attribute of Enemy which did not exist.
    
    enemy.py
    
    added an dummy input method which should be overwritten in child classes
    if needed to prevent pygame.sprite.Group.update() throwing an error

--- code/enemy.py
+++ code/enemy.py
@@ -4,7 +4,7 @@ import pygame
 
 
 class Enemy(Entity):
-    def __init__(self, pos: tuple[float,float], asset_image_root_dir_path: str, groups: list[pygame.sprite.Sprite],
+    def __init__(self, pos: tuple[float, float], asset_image_root_dir_path: str, groups: list[pygame.sprite.Sprite],
                  obstacle_sprites: pygame.sprite.Group):
 
         super().__init__(pos, asset_image_root_dir_path, groups, obstacle_sprites)
