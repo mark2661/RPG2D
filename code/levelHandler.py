0576fe086eb4f227cf0c51278f416485dc90699f
Mark <markcartwright65@gmail.com>
2023-03-09 18:32:52 +0000

    Add type hints to LevelHandler class
    type hints added to transition, transition_map,
    update_player_attributes, update_groups, observer_update, and run
    methods.

--- code/levelHandler.py
+++ code/levelHandler.py
@@ -1,5 +1,5 @@
 import pygame.display
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Optional
 from settings import *
 from level import Level
 from player import Player
@@ -32,8 +32,9 @@ class LevelHandler(Observer):
         # create the player instance that will be passed between levels
         player_spawn_position: Tuple[int, int] = tuple(map(lambda x: x // 2, self.display_surface.get_size()))
         self.player: Player = Player(player_spawn_position, PLAYER_IMAGES_FILE_PATH, [self.visible_sprites_group],
-                             self.obstacle_sprites_group, self.transition_sprites_group, self.spawn_points_group,
-                             self.current_level_code)
+                                     self.obstacle_sprites_group, self.transition_sprites_group,
+                                     self.spawn_points_group,
+                                     self.current_level_code)
 
         # pass the player instance to the current level
         self.current_level.set_player(self.player)
@@ -41,10 +42,10 @@ class LevelHandler(Observer):
         # call super constructor
         super().__init__(self.player)
 
-    def transition(self):
+    def transition(self) -> None:
         # if player has collided with a transition object
         # if self.current_level_code != self.player.get_current_level_code():
-        def transition_map():
+        def transition_map() -> None:
             # update current level code
             self.current_level_code = self.player.get_current_level_code()
 
@@ -54,7 +55,7 @@ class LevelHandler(Observer):
             # draw new map
             self.current_level.visible_sprites.regular_draw()
 
-        def update_player_attributes():
+        def update_player_attributes() -> None:
             # update group attributes
             self.update_groups()
 
@@ -70,12 +71,12 @@ class LevelHandler(Observer):
         transition_map()
         update_player_attributes()
 
-    def update_groups(self):
+    def update_groups(self) -> None:
         # get the pygame group member objects of the current level
         self.visible_sprites_group, self.obstacle_sprites_group, \
             self.transition_sprites_group, self.spawn_points_group = self.current_level.get_level_groups()
 
-    def get_spawn_point(self, spawn_point_id):
+    def get_spawn_point(self, spawn_point_id: int) -> Optional[pygame.sprite.Sprite]:
         for sprite in self.spawn_points_group:
             if sprite.get_spawn_point_id() == spawn_point_id:
                 return sprite
@@ -83,9 +84,9 @@ class LevelHandler(Observer):
         return None
 
     # Override
-    def observer_update(self):
+    def observer_update(self) -> None:
         if self.current_level_code != self.player.get_current_level_code():
             self.transition()
 
-    def run(self):
+    def run(self) -> None:
         self.current_level.run()
